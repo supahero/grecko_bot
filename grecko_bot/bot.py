825e17567f0ab4a31eb73ca91b601ec5c359ed71
Vadim <kropvad@gmail.com>
2019-12-26 03:51:32 +0300

    changed_name

--- grecko_bot/bot.py
+++ grecko_bot/bot.py
@@ -1,5 +1,5 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import logging, test
+import logging, collector
 updater = Updater(token='TOKEN', use_context=True)
 
 dispatcher = updater.dispatcher
@@ -13,7 +13,7 @@ def help(update, context):
     update.message.reply_text("Use /start to test this bot.")
 
 def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=str(test.pusher(update.message.text)))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=str(collector.pusher(update.message.text)))
 
 
 echo_handler = MessageHandler(Filters.text, echo)
